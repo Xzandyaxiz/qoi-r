81bbbb5009f6efbfdf5363a7cf848bc933908d58
Xzandyaxiz <xyandnotzaxis@gmail.com>
2024-07-16 11:10:49 +0200

    encoder progress.

--- src/img_enc.c
+++ src/img_enc.c
@@ -3,20 +3,31 @@
 #include <stdio.h>
 #include "qoir.h"
 
-#define set_last_pixel()      \
-  pixels[i] = last_pixel[0];  \
-  pixels[i+1] = last_pixel[1];\
-  pixels[i+2] = last_pixel[2];\
-  pixels[i+3] = last_pixel[3];
+#define set_last_pixel()  \
+  last_pixel = 0;         \
+  last_pixel = (          \
+    (pixels[i])         | \
+    (pixels[i+1] << 8)  | \
+    (pixels[i+2] << 16) | \
+    (pixels[i+3] << 24)   \
+  );
 
-int main()
+int8_t is_seen(uint32_t last_pixel, uint32_t *seen_pixels)
 {
-  return 0;
+  for (int8_t i = 0; i < 64; i++)
+  {
+    if (seen_pixels[i] == last_pixel)
+    {
+      return i;
+    }
+  }
+
+  return -1;
 }
 
 uint8_t *qoi_to_bytes(uint8_t *pixels, uint32_t width, uint32_t height)
 {
-  uint8_t last_pixel[4] = { 0, 0, 0, 0 };
+  uint32_t last_pixel;
   uint8_t bytes[(width * height)*4];
   uint8_t counter = 0;
   
@@ -32,13 +43,13 @@ uint8_t *qoi_to_bytes(uint8_t *pixels, uint32_t width, uint32_t height)
   
   uint8_t run_loops = 0;
 
+  uint32_t seen_pixels[64] = { 0 };
+
   for (int i = 0; i < ((width * height) * 4); i += 4)
   {
+    // QOI_OP_RUN
     // Checking run bytes
-    if (pixels[i] == last_pixel[0]   &&
-        pixels[i+1] == last_pixel[1] &&
-        pixels[i+2] == last_pixel[2] &&
-        pixels[i+3] == last_pixel[3])
+    if (pixels[i] == last_pixel)
     {
       run_loops ++;
       if (run_loops >= 62)
@@ -51,9 +62,30 @@ uint8_t *qoi_to_bytes(uint8_t *pixels, uint32_t width, uint32_t height)
       uint8_t run_byte = 0xc0;
       run_byte |= run_loops;
       bytes[counter] = run_byte;
+      counter++;
       run_byte++;
     }
 
     run_loops = 0;
+
+    // QOI_OP_LUMA
+    if ((int) abs(last_pixel[0] - pixels[i]) > 3 &&
+      (int) abs(last_pixel[1] - pixels[i+1]) > 3 &&
+      (int) abs(last_pixel[2] - pixels[i+2]) > 3)
+    {
+      set_last_pixel();
+    }
+    // QOI_OP_DIFF
+    else
+    {
+      set_last_pixel();
+    }
+
+    uint8_t index_byte = is_seen(last_pixel, &seen_pixels);
+    if (index_byte >= 0)
+    {
+      bytes[counter] = index_byte;
+      counter++;
+    }
   } 
 }
---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+  if (1)
+    printf("1\n");
+
+  if (0)
+    printf("0\n");
+}
